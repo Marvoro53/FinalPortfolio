e5369a1f6fdf4c0f3ea41c2d2a18f93c4f60b7d2
Marvoro53 <marvinlara321@gmail.com>
2021-06-28 18:45:56 -0600

    finished set up and files

--- app.js
+++ app.js
@@ -1,18 +1,70 @@
 // Require Libraries
-const express = require('express');
+var express = require('express');
+var path= require('path');
+const bodyParser= require('body-parser');
+const nodemailer = require('nodemailer');
 
 // App Setup
-const app = express();
+var app = express();
+//pug set up
+app.set('views', path.join(__dirname, 'views'));
+app.set("view engine", "pug");
 
 // Middleware
+//to make POST request 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+// to use style from public folder
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
 app.get('/', (req, res) => {
-    res.send('Hello Squirrel');
+    res.render('home');
   });
+
+app.get("/about", function(req, res){
+  res.render('about');
+});
+
+app.get("/work", function(req, res){
+  res.render('work')
+});
+
+app.get("/contact", function(req, res){
+  res.render('contact')
+});
+//nodemaiker
+//A post request for NODEMAILER https://nodemailer.com/about/
+app.post("/contact/send", function(req, res){
+  var transporter = nodemailer.createTransport({
+      service: 'Gmail',
+      auth: {
+          user: 'marvinlara321@gmail.com',
+          pass: 'Malvoro53!',
+      }
+  })
+
+//send mail
+var mailOptions = {
+  from: 'Marvin Lara <marvinlara321@gmail.com>',
+  to: 'Marvoroo53@gmail.com',
+  subject: 'Website Submission',
+  text: 'You have a submission with the following details... Name: '+req.body.name+'Email: '+req.body.email+ 'Message: '+req.body.message,
+  html: '<p>You have a submission with the following details...</p><ul><li>Name: '+req.body.name+'</li><li>Email: '+req.body.email+'</li><li>Message: '+req.body.message+'</li></ul>'
+};
+transporter.sendMail(mailOptions, function(error, info){
+  if(error){
+      console.log(error);
+      res.redirect('/');
+  } else {
+      console.log('Message Sent: '+ info.response);
+      res.redirect('/');
+  }
+});
+});
 
 // Start Server
 
 app.listen(3000, () => {
-  console.log('Gif Search listening on port localhost:3000!');
+  console.log('App is listening on localhost:3000!');
 });
